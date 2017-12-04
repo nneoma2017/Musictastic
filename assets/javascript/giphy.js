a008c4afb58d58b85a4d15e5de975b6738c3bd76
Nneoma Akubuilo <nneomaakubuilo@tests-MacBook-Pro-3.local>
2017-12-03 18:26:17 -0800

    updated

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -8,64 +8,85 @@ $(document).ready(function() {
     // append buttons to page 
 
 
-    for (var i = 0 ; i < artists.length ; i++ )  {
-        $("#artist-buttons").append("<button class='btn btn-primary' type='submit'> "+ artists[i] + " </button>");
-        console.log("buttons");
+   // function to make buttons and add to page
+  function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+    $(areaToAddTo).empty();
+
+    for (var i = 0; i < arrayToUse.length; i++) {
+      var a = $("<button>");
+      a.addClass(classToAdd);
+      a.attr("data-type", arrayToUse[i]);
+      a.text(arrayToUse[i]);
+      $(areaToAddTo).append(a);
     }
 
-
-  // create function to make buttons
-  function createButtons( textValue ) {
-        $("#artist-buttons").append("<button class='btn btn-primary' type='submit'> "+ textValue + " </button>");
-      
- 
   }
 
-  $("#artist-form").on("submit",function(event) {
-    event.preventDefault();
-      var artistText = $("#artist-input").val();
-      
-      createButtons(artistText);
-  })
-
-
-
-
-  //create function when the button is clicked for adding the artist 
-  $("#artist-buttons").on("click", ".btn", function() {
+  $(document).on("click", ".artist-button", function() {
     $("#artists").empty();
-    console.log("#artist-buttons")
+    $(".artist-button").removeClass("active");
+    $(this).addClass("active");
 
-    var artistText = $(this).html();
-    console.log(artistText);
-    
-  
-
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + artistText + "&api_key=dc6zaTOxFJmzC&limit=10";
-
+    var type = $(this).attr("data-type");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     $.ajax({
       url: queryURL,
       method: "GET"
     })
     .done(function(response) {
-      var imgUrl = response.data.image_original_url;
-      console.log(response);
+      var results = response.data;
 
-      //create for loop
-      //append images 
-      // pause and stop gifs
+      for (var i = 0; i < results.length; i++) {
+        var artistDiv = $("<div class=\"artist-item\">");
 
+        var rating = results[i].rating;
 
-     
-      
+        var p = $("<p>").text("Rating: " + rating);
+
+        var animated = results[i].images.fixed_height.url;
+        var still = results[i].images.fixed_height_still.url;
+
+        var artistImage = $("<img>");
+        artistImage.attr("src", still);
+        artistImage.attr("data-still", still);
+        artistImage.attr("data-animate", animated);
+        artistImage.attr("data-state", "still");
+        artistImage.addClass("artist-image");
+
+        artistDiv.append(p);
+        artistDiv.append(artistImage);
+
+        $("#artists").append(artistDiv);
+      }
+    });
   });
 
+  $(document).on("click", ".artist-image", function() {
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    }
+    else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
   });
 
+  $("#add-artist").on("click", function(event) {
+    event.preventDefault();
+    var newArtist = $("input").eq(0).val();
+
+    if (newArtist.length > 2) {
+      artists.push(newArtist);
+    }
+
+    populateButtons(artists, "artist-button", "#artist-buttons");
+
+  });
+
+  populateButtons(artists, "artist-button", "#artist-buttons");
 });
-
-
- 
-
- // push the new buttons
